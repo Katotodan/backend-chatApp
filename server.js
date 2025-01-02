965fa0915e698cefd181133f8d20ccbc98777e08
unknown <katotodan@gmail.com>
2025-01-02 11:14:05 +0300

    header setting

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 8000
 app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000})) 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "*"); // Replace with your React app's origin
+    res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL); // Replace with your React app's origin
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
